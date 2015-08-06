9ced8902c157a9d757221e9056df4a81fc37e4a6
dat <dat@dats-MacBook-Pro.local>
2015-08-06 18:43:43 +0700

    Initial Commit

---
+++ iRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iRich
+//
+//  Created by dat on 8/6/15.
+//  Copyright (c) 2015 dat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
